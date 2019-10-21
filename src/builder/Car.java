90f7e745aed0b09c5296393ae9eb56ff92779fb5
egnaf <mamadaliev@yahoo.com>
2019-10-21 17:49:12 +0300

    Add Car class

---
+++ src/builder/Car.java
@@ -0,0 +1,48 @@
+package builder;
+
+public class Car {
+    private String model;
+    private int year;
+    private int speed;
+
+    private Car() {}
+
+    public Car(Builder builder) {
+        this.model = builder.model;
+        this.year = builder.year;
+        this.speed = builder.speed;
+    }
+
+    public static class Builder {
+        private String model;
+        private int year;
+        private int speed;
+
+        public Builder(String model) {
+            this.model = model;
+        }
+
+        public Builder setYear(int year) {
+            this.year = year;
+            return this;
+        }
+
+        public Builder setSpeed(int speed) {
+            this.speed = speed;
+            return this;
+        }
+
+        public Car build() {
+            return new Car(this);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "model='" + model + '\'' +
+                ", year=" + year +
+                ", speed=" + speed +
+                '}';
+    }
+}
