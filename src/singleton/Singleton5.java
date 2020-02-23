24d7d9d540f9013fab82a80debdc99c931dc523c
egnaf <egnaf@yahoo.com>
2020-02-23 16:01:51 +0300

    Add non lazy singleton

---
+++ src/singleton/Singleton5.java
@@ -0,0 +1,13 @@
+package singleton;
+
+public class Singleton5 {
+
+    private static Singleton5 instance = new Singleton5();
+
+    private Singleton5() {
+    }
+
+    public static Singleton5 getInstance() {
+        return instance;
+    }
+}
