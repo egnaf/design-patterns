eb08846a23cd69333aee9b0b5e87145462ae4199
egnaf <egnaf@yahoo.com>
2020-02-25 16:02:32 +0300

    Add main method to running

--- src/singleton/Main.java
+++ src/singleton/Main.java
@@ -5,10 +5,11 @@ public class Main {
     public static void main(String[] args) {
 
         /* create instance of the Singleton class */
-        Singleton singleton1 = Singleton.getInstance();
-        Singleton singleton2 = Singleton.getInstance();
 
-        System.out.println(singleton1); // singleton.Singleton@610455d6
-        System.out.println(singleton2); // singleton.Singleton@610455d6
+        System.out.println( Singleton1.getInstance() );
+        System.out.println( Singleton2.getInstance() );
+        System.out.println( Singleton3.getInstance() );
+        System.out.println( Singleton4.getInstance() );
+        System.out.println( Singleton5.getInstance() );
     }
 }
